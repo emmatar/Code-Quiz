369be0ecbb7793654a402c21caf6d40adbc1fbbd
Elijah Matar <m.matar515@gmail.com>
2024-01-16 23:33:38 -0600

    edits to code quiz javascript. making quesitons appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,199 @@
-var startBtn = document.querySelector("#startBtn")
-var timeEl = document.querySelector(".time");
+var startBtn = document.querySelector("#startBtn");
+var timerTxt = document.querySelector(".timer-text");
+var timerCount = document.querySelector("#timer-count");
+var mainEl = document.querySelector("main");
+var firstPage = document.querySelector("#gameStart");
+var question = document.querySelector("#question")
+var answer = document.querySelector(".answer")
+var questionText = document.getElementById("content");
 var timer;
-var timerCount;
-var isWin = false;
-
+var seconds = 100;
+var index = 0;
 var questions = [
     {
         question: "Javascript is an _______ language?",
-        A: "Object-Based",
-        B: "Object-Oriented",
-        C: "Procedural",
-        D: "None of the Above",
-        Answer: "A"
+        answers: [
+        {
+            answer: "Object-Based",
+            isAnswer: true
+        },
+        {
+            answer: "Object-Oriented",
+            isAnswer: false
+        },
+        {
+            answer: "Procedural",
+            isAnswer: false
+        },
+        {
+            answer: "None of the Above",
+            isAnswer: false
+        }
+        ]
     },
     {
         question: "Which of the following methods is used to access HTML elements using Javascript?",
-        A: "getElementbyId()",
-        B: "getElementsByClassName",
-        C: "Both A and B",
-        D: "None of the Above",
-        Answer: "C"
+        answers: [
+        {
+            answer: "getElementbyId()",
+            isAnswer: false
+        },
+        {
+            answer: "getElementsByClassName",
+            isAnswer: false
+        },
+        {
+            answer: "Both A and B",
+            isAnswer: true
+        },
+        {
+            answer: "None of the Above",
+            isAnswer: false
+        }
+        ]
     },
     {
         question: "Which of the following methods can be used to display data in some form using Javascript?",
-        A: "document.write()",
-        B: "console.log()",
-        C: "window.aler()",
-        D: "All of the Above",
-        Answer: "D"
+        answers: [
+        {
+            answer: "document.write()",
+            isAnswer: false
+        },
+        {
+            answer: "console.log()",
+            isAnswer: false
+        },
+        {
+            answer: "window.alert()",
+            isAnswer: false
+        },
+        {
+            answer: "All of the Above",
+            isAnswer: true
+        }
+        ]
     },
     {
         question: "How can a datatype be declared to be a constant type?",
-        A: "var",
-        B: "const",
-        C: "let",
-        D: "constant",
-        Answer: "B"
+        answers: [
+        {
+            answer: "var",
+            isAnswer: false
+        },
+        {
+            answer: "const",
+            isAnswer: true
+        },
+        {
+            answer: "let",
+            isAnswer: false
+        },
+        {
+            answer: "constant",
+            isAnswer: false
+        }        
+        ]
     },
     {
         question: "How do we put Javascript inside HTML?",
-        A: "<js>",
-        B: "<scripting>",
-        C: "<javascript>",
-        D: "<script>",
-        Answer: "D"
+        answers: [
+        {
+            answer: "<js>",
+            isAnswer: false
+        },
+        {
+            answer: "<scripting>",
+            isAnswer: false
+        },
+        {
+            answer: "<javascript>",
+            isAnswer: false
+        },
+        {
+            answer: "<script>",
+            isAnswer: true
+        }    
+        ]
     }
 ]
 
+function startQuiz() {
+    // Hide start page
+    $("#gameStart").removeClass("active");
+    // start timer
+    startTimer();
+    // unhide question section
+    $("#question").addClass("active");
+    // display question
+    displayQuestion();
+}
 function startTimer() {
     timer = setInterval(function () {
-        timeEl.textContent = parseInt(timerCount);
-        timerCount--;
-        if (timerCount >=  0) {
-            if (isWin && timerCount > 0) {
-                clearInterval(timer);
-                winGame();
-            }
-        }
-        if (timerCount === 0) {
+        // timeEl.textContent = timerCount;
+        seconds--;
+        timerCount.textContent = seconds;
+        if (seconds === 0) {
             clearInterval(timer);
-            loseGame();
         }
-        
     }, 1000);
 }
-// Created the timer for the game next is to check out winGame and loseGame. Reference 04-APIs-28(Stu-Mini-Proj)
-// TIMER WONT START. I really believe you. You got this.
-startBtn.addEventListener("click", startTimer);
 
+function displayQuestion () {
+    // get the question
+    var currentQuestion = questions[index];
+    // display question text
+    questionText.textContent = currentQuestion.question;
+    // display answer options
+    for (let i = 0; i < currentQuestion.answers.length; i++) {
+        var answerOptions = currentQuestion.answers[i].answer;
+        // create a button; (which is declared 'button' in js)
+        var button = document.createElement("button");
+        // put the text in the button
+        button.textContent = answerOptions;
+        // put button on page
+        answer.append(button);
+        // click event to initiate checkAnswer function
+        button.addEventListener("click", checkAnswer.bind(currentQuestion.answers[i].isAnswer));
+        // NEED TO FIGURE OUT HOW TO ROTATE THROUGH THE "answers[]" LIKE
+        // LINE 143 & 145 WITH THE EACH QUESTION OBJECT IN THE ARRAY. 
+    } 
+}
+
+function checkAnswer(event) {
+    // currentQuestion.answers[i].isAnswer
+    var realAnswer = document.createElement("h2");
+    if (this.valueOf()) {
+            realAnswer.textContent = "Correct!"
+            question.append(realAnswer);
+            setTimeout(() => {
+                realAnswer.textContent = " "
+            }, 1000);
+    } else {
+        seconds -= 10;
+        realAnswer.textContent = "Wrong:("
+        question.append(realAnswer);
+        setTimeout(() => {
+            realAnswer.textContent = " "
+        }, 1000);
+    }
+    answer.textContent = " "
+    checkEnd();
+}
+
+function checkEnd () {
+    if (index <= 4) {
+        index++;
+        displayQuestion();
+    } else if (index > 4) {
+        gameEnd();
+    } 
+}
+
+function gameEnd () {
+    console.log("gameend");
+    question.textContent = " "
+
+}
+
+// Created the timer for the game next is to check out winGame and loseGame. Reference 04-APIs-28(Stu-Mini-Proj)
+startBtn.addEventListener("click", startQuiz);
