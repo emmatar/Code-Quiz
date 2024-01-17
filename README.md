2eb2ecffd24cb237441444bb7e4305aad19d6e07
Elijah Matar <m.matar515@gmail.com>
2024-01-17 17:29:44 -0600

    HTML/CSS/JavaScript edited to diplsay questions after starting quiz. Score saves to local storate using JSON

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Code-Quiz
+
+## Description
+
+Welcome to the Code Quiz! Would you like to test your knowledge on some Javascript Basics? Well, if you are, you're exactly where you need to be. The Code quiz is a timed quiz that will dock the time to complete the quiz, if you answer incorrectly! *slow but intense music plays*
+
+*Click* that "Start Quiz" button and answer the questions. Your score results will appear at the end of the game, which will be saved on your computer for a chance to top your high score in the future! Enjoy!
+
+ 
+## Installation
+
+N/A
+
+## Usage
+
+
+
+To Begin: *Click* the "Start Quiz" button, and a the game will start displaying quiz questions that have multiple choice answers to choose from. Be careful though, if you were to answer a question incorrectly, the game will dock 20 seconds from your time to finish the quiz.
+
+After answering all of the questions, the quiz results will appear and an input field will be displayed right under the score amount. The input's function is to save the users *Name* and *Score* on their local storage, so they can come back and beat their initial attempts.
+
+
+
+Click this link to find out: 
+
+![Screenshot1 of Code Quiz](/assets/images/img1.png)
+![Screenshot2 of Code Quiz](/assets/images/img2.png)
+![Screenshot3 of Code Quiz](/assets/images/img3.png)
+
+## License
+
+ N/A
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,197 +3,234 @@ var timerTxt = document.querySelector(".timer-text");
 var timerCount = document.querySelector("#timer-count");
 var mainEl = document.querySelector("main");
 var firstPage = document.querySelector("#gameStart");
-var question = document.querySelector("#question")
-var answer = document.querySelector(".answer")
+var question = document.querySelector("#question");
+var answer = document.querySelector(".answer");
 var questionText = document.getElementById("content");
+var finalPage = document.getElementById("gameEnd");
 var timer;
 var seconds = 100;
 var index = 0;
+var score = document.getElementById("score");
+var initials = document.getElementById("initials");
+var btnInitials = document.getElementById("btn-initials");
+var scoreTally = 0;
+
 var questions = [
-    {
-        question: "Javascript is an _______ language?",
-        answers: [
-        {
-            answer: "Object-Based",
-            isAnswer: true
-        },
-        {
-            answer: "Object-Oriented",
-            isAnswer: false
-        },
-        {
-            answer: "Procedural",
-            isAnswer: false
-        },
-        {
-            answer: "None of the Above",
-            isAnswer: false
-        }
-        ]
-    },
-    {
-        question: "Which of the following methods is used to access HTML elements using Javascript?",
-        answers: [
-        {
-            answer: "getElementbyId()",
-            isAnswer: false
-        },
-        {
-            answer: "getElementsByClassName",
-            isAnswer: false
-        },
-        {
-            answer: "Both A and B",
-            isAnswer: true
-        },
-        {
-            answer: "None of the Above",
-            isAnswer: false
-        }
-        ]
-    },
-    {
-        question: "Which of the following methods can be used to display data in some form using Javascript?",
-        answers: [
-        {
-            answer: "document.write()",
-            isAnswer: false
-        },
-        {
-            answer: "console.log()",
-            isAnswer: false
-        },
-        {
-            answer: "window.alert()",
-            isAnswer: false
-        },
-        {
-            answer: "All of the Above",
-            isAnswer: true
-        }
-        ]
-    },
-    {
-        question: "How can a datatype be declared to be a constant type?",
-        answers: [
-        {
-            answer: "var",
-            isAnswer: false
-        },
-        {
-            answer: "const",
-            isAnswer: true
-        },
-        {
-            answer: "let",
-            isAnswer: false
-        },
-        {
-            answer: "constant",
-            isAnswer: false
-        }        
-        ]
-    },
-    {
-        question: "How do we put Javascript inside HTML?",
-        answers: [
-        {
-            answer: "<js>",
-            isAnswer: false
-        },
-        {
-            answer: "<scripting>",
-            isAnswer: false
-        },
-        {
-            answer: "<javascript>",
-            isAnswer: false
-        },
-        {
-            answer: "<script>",
-            isAnswer: true
-        }    
-        ]
-    }
-]
+  {
+    question: "A variety of grouped properties, that have a name and value is called:",
+    answers: [
+      {
+        answer: "Array",
+        isAnswer: false,
+      },
+      {
+        answer: "String",
+        isAnswer: false,
+      },
+      {
+        answer: "Object",
+        isAnswer: true,
+      },
+      {
+        answer: "None of the Above",
+        isAnswer: false,
+      },
+    ],
+  },
+  {
+    question:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    answers: [
+      {
+        answer: "getElementbyId()",
+        isAnswer: false,
+      },
+      {
+        answer: "getElementsByClassName",
+        isAnswer: false,
+      },
+      {
+        answer: "Both A and B",
+        isAnswer: true,
+      },
+      {
+        answer: "None of the Above",
+        isAnswer: false,
+      },
+    ],
+  },
+  {
+    question:"Which of the following methods can be used to display data using Javascript?",
+    answers: [
+      {
+        answer: "document.write()",
+        isAnswer: false,
+      },
+      {
+        answer: "console.log()",
+        isAnswer: false,
+      },
+      {
+        answer: "window.alert()",
+        isAnswer: false,
+      },
+      {
+        answer: "All of the Above",
+        isAnswer: true,
+      },
+    ],
+  },
+  {
+    question: "How can a datatype be declared to be a constant type?",
+    answers: [
+      {
+        answer: "var",
+        isAnswer: false,
+      },
+      {
+        answer: "const",
+        isAnswer: true,
+      },
+      {
+        answer: "let",
+        isAnswer: false,
+      },
+      {
+        answer: "constant",
+        isAnswer: false,
+      },
+    ],
+  },
+  {
+    question: "What is the proper tag to insert Javascript inside HTML?",
+    answers: [
+      {
+        answer: "<js>",
+        isAnswer: false,
+      },
+      {
+        answer: "<scripting>",
+        isAnswer: false,
+      },
+      {
+        answer: "<javascript>",
+        isAnswer: false,
+      },
+      {
+        answer: "<script>",
+        isAnswer: true,
+      },
+    ],
+  },
+];
 
 function startQuiz() {
-    // Hide start page
-    $("#gameStart").removeClass("active");
-    // start timer
-    startTimer();
-    // unhide question section
-    $("#question").addClass("active");
-    // display question
-    displayQuestion();
+  // Hide start page
+  $("#gameStart").addClass("hideMe");
+  // start timer
+  startTimer();
+  // unhide question section
+  $("#question").removeClass("hideMe");
+  // display question
+  displayQuestion();
 }
 function startTimer() {
-    timer = setInterval(function () {
-        // timeEl.textContent = timerCount;
-        seconds--;
-        timerCount.textContent = seconds;
-        if (seconds === 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
+  timer = setInterval(function () {
+    // timeEl.textContent = timerCount;
+    seconds--;
+    timerCount.textContent = seconds;
+    if (seconds === 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
 }
 
-function displayQuestion () {
+function displayQuestion() {
+  if (questions[index]) {
+    // var currentQuestion = questions[index];
     // get the question
-    var currentQuestion = questions[index];
+    // currentQuestion = questions[index];
     // display question text
-    questionText.textContent = currentQuestion.question;
+    questionText.textContent = questions[index].question;
     // display answer options
-    for (let i = 0; i < currentQuestion.answers.length; i++) {
-        var answerOptions = currentQuestion.answers[i].answer;
-        // create a button; (which is declared 'button' in js)
-        var button = document.createElement("button");
-        // put the text in the button
-        button.textContent = answerOptions;
-        // put button on page
-        answer.append(button);
-        // click event to initiate checkAnswer function
-        button.addEventListener("click", checkAnswer.bind(currentQuestion.answers[i].isAnswer));
-        // NEED TO FIGURE OUT HOW TO ROTATE THROUGH THE "answers[]" LIKE
-        // LINE 143 & 145 WITH THE EACH QUESTION OBJECT IN THE ARRAY. 
-    } 
+    for (let questionAnswer of questions[index].answers) {
+      var answerOptions = questionAnswer.answer;
+      // create a button; (which is declared 'button' in js)
+      var button = document.createElement("button");
+      // put the text in the button
+      button.setAttribute("data-correct", questionAnswer.isAnswer);
+      button.textContent = answerOptions;
+      // put button on page
+      answer.append(button);
+      // click event to initiate checkAnswer function
+      // NEED TO FIGURE OUT HOW TO ROTATE THROUGH THE "answers[]" LIKE
+      // LINE 143 & 145 WITH THE EACH QUESTION OBJECT IN THE ARRAY.
+    }
+  } else {
+    gameEnd();
+  }
 }
 
 function checkAnswer(event) {
-    // currentQuestion.answers[i].isAnswer
+  // currentQuestion.answers[i].isAnswer
+  if (event.target.matches("button")) {
     var realAnswer = document.createElement("h2");
-    if (this.valueOf()) {
-            realAnswer.textContent = "Correct!"
-            question.append(realAnswer);
-            setTimeout(() => {
-                realAnswer.textContent = " "
-            }, 1000);
+    if (event.target.dataset.correct === "true") {
+      realAnswer.textContent = "Correct!";
+      question.append(realAnswer);
+      setTimeout(() => {
+        realAnswer.textContent = " ";
+      }, 500);
     } else {
-        seconds -= 10;
-        realAnswer.textContent = "Wrong:("
-        question.append(realAnswer);
-        setTimeout(() => {
-            realAnswer.textContent = " "
-        }, 1000);
+      seconds -= 20;
+      realAnswer.textContent = "Wrong:(";
+      question.append(realAnswer);
+      setTimeout(() => {
+        realAnswer.textContent = " ";
+      }, 500);
     }
-    answer.textContent = " "
-    checkEnd();
+    answer.textContent = " ";
+    nextStep();
+  }
 }
 
-function checkEnd () {
-    if (index <= 4) {
-        index++;
-        displayQuestion();
-    } else if (index > 4) {
-        gameEnd();
-    } 
+function nextStep() {
+  index++;
+  displayQuestion();
 }
 
-function gameEnd () {
-    console.log("gameend");
-    question.textContent = " "
+function gameEnd() {
+  clearInterval(timer);
+  question.classList.add("hideMe");
+  finalPage.classList.remove("hideMe");
+  score.textContent = seconds;
+  scoreTally = seconds;
+}
 
+function saveScore() {
+  var savedName = initials.value;
+  var userScore = {
+    score: seconds,
+    savedName: savedName,
+  };
+  var userScoreTally = JSON.parse(localStorage.getItem("scores")) || [];
+  userScoreTally.push(userScore);
+  localStorage.setItem("scores", JSON.stringify(userScoreTally));
+  displayHighScores();
+}
+function displayHighScores() {
+    const scorePage = document.getElementById("scorePage")
+    const highScores = JSON.parse(localStorage.getItem("scores"))
+    const scoresList = document.createElement("ul")
+    highScores.forEach(function(score) {
+        const scoreEl = document.createElement("li")
+        scoreEl.innerHTML = `<span class="scoreInitials">Initials: ${score.savedName}</span> <span class="scoreTally">Score: ${score.score}</span>`
+        scoresList.appendChild(scoreEl)
+    });
+    scorePage.appendChild(scoresList);
 }
 
 // Created the timer for the game next is to check out winGame and loseGame. Reference 04-APIs-28(Stu-Mini-Proj)
 startBtn.addEventListener("click", startQuiz);
+question.addEventListener("click", checkAnswer);
+btnInitials.addEventListener("click", saveScore)
